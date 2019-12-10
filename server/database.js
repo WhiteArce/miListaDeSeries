5c33dfc118b3fdf5bec0cc6f9562deac978f297a
WhiteArce <whiteheim.09@gmail.com>
2019-12-09 22:15:21 -0700

    12/09/2019

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const URI = 'mongodb://localhost/series-crud';
+const URI = 'mongodb+srv://WhiteArce:<maplerce>@listaseries-yiv00.azure.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
     .then(db => console.log("Conectado a la base de datos"))
