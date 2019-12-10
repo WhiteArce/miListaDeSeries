e7c988d82d4322dea802100fa78bbc40396e96d9
WhiteArce <whiteheim.09@gmail.com>
2019-12-09 20:02:21 -0700

    12/09/2019

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const cors = require('cors');
 
 const { mongoose } = require('./database');
 
@@ -12,6 +13,7 @@ app.set('port', process.env.PORT || 3000);
 
 
 //Middleware
+app.use(cors({origin: 'http://localhost:4200'}));
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -19,6 +21,8 @@ app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse appl
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
+
 
 //Routes
 app.use('/api/series', require('./routes/series.routes'));
