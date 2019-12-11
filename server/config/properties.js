9cd38d02dfeb172b03fc2c733ffc49d423e2068d
WhiteArce <whiteheim.09@gmail.com>
2019-12-11 13:02:35 -0700

    12/11/2019

--- server/config/properties.js
+++ server/config/properties.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 3000,
-    //DB: 'mongodb://localhost:27017/miLista'
-    DB: 'mongodb+srv://WhiteArce:maplerce@listaseries-yiv00.azure.mongodb.net/test?retryWrites=true&w=majority'
+    DB: 'mongodb://localhost:27017/miLista'
+    //DB: 'mongodb+srv://WhiteArce:maplerce@listaseries-yiv00.azure.mongodb.net/test?retryWrites=true&w=majority'
 }
